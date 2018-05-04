964cd6f1c7809ae483076f1cfe9f554cc708cae2
Ivan Rep <ivan.rep2@gmail.com>
2018-05-04 20:42:24 +0200

    ReusableDataSource initial commit

---
+++ ReusableDataSource/ReusableDataSource.h
@@ -0,0 +1,19 @@
+//
+//  ReusableDataSource.h
+//  ReusableDataSource
+//
+//  Created by Ivan Rep on 04/05/2018.
+//  Copyright © 2018 Rep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReusableDataSource.
+FOUNDATION_EXPORT double ReusableDataSourceVersionNumber;
+
+//! Project version string for ReusableDataSource.
+FOUNDATION_EXPORT const unsigned char ReusableDataSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReusableDataSource/PublicHeader.h>
+
+
